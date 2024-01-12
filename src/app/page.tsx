73a16f92ecf421cd1dc460a5d22fbe0fc6573633
5war00p <maddisaiswaroop@gmail.com>
2024-01-12 12:28:40 +0530

    documentation and dynamic image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
 import Navbar from "@/components/navbar";
-import Image from "next/image";
 import View from "@/components/view";
 
 export default function Home() {
   return (
     <>
-      <div className="fixed w-[100vw] h-[100vh] overflow-hidden z-[-1] top-0 left-0">
-        <Image
-          alt="travel"
-          src={`/bgs/${Math.floor(Math.random() * 9)}.jpg`}
-          fetchPriority="high"
-          fill
-        />
-      </div>
       <Navbar />
       <View />
     </>
--- src/components/view.tsx
+++ src/components/view.tsx
@@ -2,11 +2,23 @@
 import { useSearchParams } from "next/navigation";
 import { ResultsView } from "./results-view";
 import { InitialView } from "./initial-view";
+import Image from "next/image";
 
 export default function View() {
   const searchParams = useSearchParams();
   const q = searchParams.get("q");
 
-  if (q) return <ResultsView />;
-  return <InitialView />;
+  return (
+    <>
+      <div className="fixed w-[100vw] h-[100vh] overflow-hidden z-[-1] top-0 left-0">
+        <Image
+          alt="travel"
+          src={`/bgs/${Math.floor(Math.random() * 9)}.jpg`}
+          fetchPriority="high"
+          fill
+        />
+      </div>
+      {q ? <ResultsView /> : <InitialView />}
+    </>
+  );
 }
